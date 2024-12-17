ad7c67486596e549e18ce4cc5aaa9e432978fd78
Crosby <crosbysayan2023@gmail.com>
2024-12-17 16:25:59 -0500

    make files and more structs and enums

--- parser.cpp
+++ parser.cpp
@@ -1,8 +1,11 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
 // handles the json files of the MBTA API and pulls relevant data.
 //Bus Stop: https://api-v3.mbta.com/stops/1315
 //Green Line: https://api-v3.mbta.com/stops/70254
 
+// Predictions: https://api-v3.mbta.com/predictions?filter[stop]=1315
 //     /Here
 //    /
 // - [] - [=-=-=] - []
@@ -14,14 +17,28 @@ enum Vehichle {
     BUS,
     FERRY
 };
-//Define what a transport is
-struct Transport
+
+enum Status {
+    INCOMING_AT, //About to arrive at thee stop
+    STOPPED_AT, //The Vehichle is standing at the stop.
+    IN_TRANSIT_TO //The Vehichle is in between stops (in stop_sequence it will say n+1 what it was from)
+}
+
+//A Ride is defined as public transportation module that will
+//get a client from their TO stop to their FROM stop.
+//Holds information about the id in the
+struct Ride
 {
     Vehichle type; //Type of vehicle
-    char * id; //c-string id.
+    int id; //all id's are numbers with the starting prefix y.
+
     unsigned int arrival; //Time of arrival (should never be negative)
-    //How full things are looking
 };
+
+struct Stop {
+    int id;
+    int stop_sequence;
+}
 
 //To Do:
 // FIFO List that holds all the currently new Transports.
@@ -29,3 +46,7 @@ struct Transport
 // Print the data out.
 // Some kind of scheduler that knocks off items that arrival is past current time.
 
+int main() {
+    cout << "Hello World" << endl;
+    return 0;
+}
