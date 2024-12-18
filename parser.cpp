1be2336422d4c408729acb4b02fda6f0477315a2
Crosby <crosbysayan2023@gmail.com>
2024-12-17 21:40:05 -0500

    Outline of Ride data

--- parser.cpp
+++ parser.cpp
@@ -18,22 +18,35 @@ enum Vehichle {
     FERRY
 };
 
+enum Occupancy {
+    EMPTY,
+    MANY_SEATS_AVAILABLE,
+    FEW_SEATS_AVAILABLE,
+    STANDING_ROOM_ONLY,
+    CRUSHED_STANDING_ROOM_ONLY,
+    FULL,
+    NOT_ACCEPTING_PASSENGERS,
+    NO_DATA_AVAILABLE
+};
+
 enum Status {
     INCOMING_AT, //About to arrive at thee stop
     STOPPED_AT, //The Vehichle is standing at the stop.
     IN_TRANSIT_TO //The Vehichle is in between stops (in stop_sequence it will say n+1 what it was from)
-}
+};
 
 //A Ride is defined as public transportation module that will
 //get a client from their TO stop to their FROM stop.
 //Holds information about the id in the
 struct Ride
 {
-    Vehichle type; //Type of vehicle
-    int id; //all id's are numbers with the starting prefix y.
-
-    unsigned int arrival; //Time of arrival (should never be negative)
+    Vehichle type;          //Type of vehicle
+    Occupancy occup_status; //The fullness of the vehichle TBI
+    Status train_status;    //The status of the train between stops.
+    string id;              //The vehichles tag
+    int arrival;            //Time of arrival
 };
+typedef Ride * p_Ride;
 
 struct Stop {
     int id;
