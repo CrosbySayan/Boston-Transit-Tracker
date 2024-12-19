bd0758fdeab26451fcc28a9da2c37f5dd9517de4
Crosby <crosbysayan2023@gmail.com>
2024-12-18 22:34:27 -0500

    pop, push and empty fixed

--- parser.cpp
+++ parser.cpp
@@ -19,7 +19,8 @@ struct node {
 };
 
 struct queue {
-    int MAX_LENGTH;
+    unsigned int MAX_LENGTH;
+    unsigned int current_length = 0;
     node * head;
     node * tail;
 };
@@ -30,6 +31,7 @@ bool empty(struct queue * queue) {
 
 void push(struct queue * queue, p_Ride elem) {
     node * new_elem = new node{elem, NULL};
+    queue->current_length++;
     if(empty(queue)) {
         queue->head = new_elem;
         queue->tail = new_elem;
@@ -43,10 +45,15 @@ p_Ride pop(struct queue * queue) {
     if(empty(queue)) {
         return NULL;
     }
-
-    
+    node * tmp = queue->head;
+    queue->head = queue->head->next;
+    tmp->next = NULL;
+    p_Ride elem = tmp->elem;
+    delete tmp;
+    return elem;
 }
 
+queue current = {4, 0, NULL, NULL};
 //To Do:
 // FIFO List that holds all the currently new Transports.
 // Build Parser to pull to create new Transports at a set interval.
@@ -55,7 +62,11 @@ p_Ride pop(struct queue * queue) {
 
 int main() {
     Ride r = {BUS, NO_DATA_AVAILABLE, INCOMING_AT, "y1234", 1205};
-
-    cout << r.arrival << endl;
+    Ride b = {LIGHT_RAIL, NO_DATA_AVAILABLE, STOPPED_AT, "G-1302", 1206};
+    push(&current, &r);
+    push(&current, &b);
+    pop(&current);
+    cout << current.head->elem->id << endl;
+    cout << current.tail->elem->id << endl;
     return 0;
 }
